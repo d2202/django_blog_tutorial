8ed86b1890256389d4ee644feee7ca85f981572a
Denis <denybf.wrk@gmail.com>
2021-02-07 19:28:13 +0300

    fixed naming in settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Heroku: update database configuration from $DATABASE_URL
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABSES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
