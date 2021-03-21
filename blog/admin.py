2e3048ba89215c6ca197eb9af5cb113d83efc3e4
Denis <denybf.wrk@gmail.com>
2021-03-21 23:22:35 +0300

    added ckeditor to admin post form

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from django import forms
 from .models import Post
+from ckeditor.widgets import CKEditorWidget
 
-admin.site.register(Post)
+class PostAdminForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+admin.site.register(Post, PostAdmin)
--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Post
+from ckeditor.widgets import CKEditorWidget
 
 class PostForm(forms.ModelForm):
+    text = forms.CharField(widget=CKEditorWidget())
 
     class Meta:
         model = Post
